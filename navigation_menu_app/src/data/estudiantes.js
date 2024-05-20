7b0cc02fb8c7a2facdd1a96cf89b0c51c2b7d4cd
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-05-20 12:04:10 -0600

    StudentsScreen prueba

---
+++ navigation_menu_app/src/data/estudiantes.js
@@ -0,0 +1,18 @@
+const Estudiante = [
+    {
+        id: 1,
+        image: require('../img/estudiante2.jpg'),
+        name: 'Susan Abigail Castillo Oliva',
+        description: 'Susanita la estudiante más inteligente del ricaldone, con habilidades en cualquier tecnología',
+        estado: true
+    },
+    {
+        id: 2,
+        image: require('../img/estudiante1.jpg'),
+        name: 'Eduardo René Cubias Morán',
+        description: 'Estudiante mega guapo del instito Técnico Ricaldone, con habilidades de guapura extrema',
+        estado: false
+    },
+]
+
+export default Estudiante;
--- navigation_menu_app/src/screens/StudentsScreen.js
+++ navigation_menu_app/src/screens/StudentsScreen.js
@@ -1,21 +1,95 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text, FlatList, Image } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Image, ScrollView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import estudiante from '../data/estudiantes';
 
-const EstudiantesScreen= () => {
+const StudentsScreen= () => {
 
     return (
         <View style={styles.container}>
-            <Text>Lista de estudiantes</Text>
+            <ScrollView>
+            <View>
+                <FlatList
+                    data={estudiante}
+                    renderItem={({ item }) => (
+                        <View style={styles.cardContainer}>
+                            <Image source={item.image} style={styles.image} />
+                            <Text style={styles.title}>{item.name}</Text>
+                            <Text style={styles.info}>{item.description}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.id}
+                    contentContainerStyle={styles.flatListContainer}
+                />
+                
+            </View>
+            <View style={styles.height}></View>
+            </ScrollView>
         </View>
     );
 };
 
-export default EstudiantesScreen;
+export default StudentsScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }
+    container: {
+        paddingTop: 20,
+        backgroundColor: 'white'
+    },
+    flatListContainer: {
+        padding: 5,
+        flexDirection: 'row', 
+        flexWrap: 'wrap',
+        justifyContent: 'space-around', 
+    },
+    cardContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        marginBottom: 25,
+        width: 180,
+        height: 300,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        alignItems:'center'
+    },
+    image: {
+        width: 180,
+        height: 100,
+        marginBottom: 10,
+        borderRadius: 8
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    mainTitle: {
+        backgroundColor: '#E7CFFC',
+        padding: 10,
+        textAlign: 'center',
+        marginBottom: 20,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'purple'
+    },
+    texto:{
+        marginHorizontal:15
+    },
+    contenedor: {
+        alignItems: 'center',
+    },
+    info:{
+        padding:10
+    },
+    height: {
+        height: 80
+    }
 });
